4bcece2efd541ca3a919d5a33381604b1f27350b
PAVAN KUMAR <44501966+pavankumar0001@users.noreply.github.com>
2022-03-06 15:27:13 +0530

    Add files via upload

---
+++ 2march assignment/gallery.js
@@ -0,0 +1,61 @@
+
+
+
+var counter=0;
+function like()
+{
+    counter+=1;
+    render();
+}
+function disLike()
+{
+    counter-=1;
+    render();
+}
+function render()
+{
+    var disp=document.getElementById("op-text");
+    disp.innerText = counter;
+    
+   
+
+}
+var counter=0;
+function like1()
+{
+    counter+=1;
+    render1();
+}
+function disLike1()
+{
+    counter-=1;
+    render1();
+}
+function render1()
+{
+    var disp1=document.getElementById("op-text1");
+    disp1.innerText = counter;
+    
+   
+
+}
+var counter=0;
+function like2()
+{
+    counter+=1;
+    render2();
+}
+function disLike2()
+{
+    counter-=1;
+    render2();
+}
+function render2()
+{
+    var disp2=document.getElementById("op-text2");
+    disp2.innerText = counter;
+    
+   
+
+}
+

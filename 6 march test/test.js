4378f389e3fbad79c14f6b8476a9db2a947108f5
PAVAN KUMAR <44501966+pavankumar0001@users.noreply.github.com>
2022-03-06 16:03:37 +0530

    Add files via upload

---
+++ 6 march test/test.js
@@ -0,0 +1,35 @@
+function Display()
+{
+    var anc=document.getElementById("hiddencontainer1");
+    anc.style.display="block";
+}
+function hide()
+{
+    var hides=document.getElementById("hiddencontainer1");
+    hides.style.display="none";
+}
+
+
+function Display2()
+{
+    var anc=document.getElementById("hiddencontainer2");
+    anc.style.display="block";
+}
+function hide2()
+{
+    var hides=document.getElementById("hiddencontainer2");
+    hides.style.display="none";
+}
+
+function Display3()
+{
+    var anc=document.getElementById("hiddencontainer3");
+    anc.style.display="block";
+}
+function hide3()
+{
+    var hides=document.getElementById("hiddencontainer3");
+    hides.style.display="none";
+}
+
+
